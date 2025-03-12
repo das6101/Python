67aca222ec6c2c16b8cdf889dbc3fd419361f867
Дарья Кириллова <das6101@yandex.ru>
2025-03-13 00:55:50 +0300

    homework

---
+++ HW17.py
@@ -0,0 +1,9 @@
+multipl = 1
+n = 1
+for n in range(1, 11):
+    if n % 2 == 0:
+        continue
+    multipl *= n
+print(multipl)
+
+
---
+++ Homework17.py
@@ -0,0 +1,7 @@
+i = int
+for i in range(1,11,1):
+    print()
+    for j in range(1,11,1):
+        print(f'{i} * {j} = {i * j}')
+
+
